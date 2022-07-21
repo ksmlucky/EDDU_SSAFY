03aa8136a5945ff8fee51814bc8f51e37df8d938
김지수 <kimjs1995@naver.com>
2022-07-21 09:58:46 +0900

    pages 폴더 생성 및 파일 이동, Homepage.js 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 /** @format */
-import Login from "./Login";
-import Signup from "./Signup";
-import {Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="/Signup" element={<Signup></Signup>}></Route>
       </Routes>
     </div>
   );
