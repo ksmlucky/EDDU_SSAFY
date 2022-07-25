7d185cff2b76a4fca9d8a39708c02e71f87f1553
최영선 <vnddns26@naver.com>
2022-07-25 14:33:41 +0900

    store 생성

---
+++ frontend/src/redux/user.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    value: {
+      name: "",
+      id: "",
+      email: "",
+      tel: "",
+      position: "",
+    },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { login } = userSlice.actions;
+
+export default userSlice.reducer;
