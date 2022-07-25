037fd48ff9d857e8419846b9e175db50886abe46
최진욱 <choi940923@naver.com>
2022-07-25 10:17:05 +0900

    0724

--- backend-java/src/main/generated/com/ssafy/db/entity/QUser.java
+++ backend-java/src/main/generated/com/ssafy/db/entity/QUser.java
@@ -19,13 +19,15 @@ public class QUser extends EntityPathBase<User> {
 
     public static final QUser user = new QUser("user");
 
-    public final StringPath department = createString("department");
+    public final StringPath email = createString("email");
 
     public final StringPath name = createString("name");
 
     public final StringPath password = createString("password");
 
     public final StringPath position = createString("position");
+
+    public final StringPath tel = createString("tel");
 
     public final StringPath userId = createString("userId");
 
--- backend-java/src/main/java/com/ssafy/db/repository/UserRepositorySupport.java
+++ backend-java/src/main/java/com/ssafy/db/repository/UserRepositorySupport.java
@@ -24,4 +24,11 @@ public class UserRepositorySupport {
         if(user == null) return Optional.empty();
         return Optional.ofNullable(user);
     }
+
+    public boolean findByUserIdEquals(String userId) {
+        User user = jpaQueryFactory.select(qUser).from(qUser)
+                .where(qUser.userId.eq(userId)).fetchOne();
+        if(user == null) return true;
+        return false;
+    }
 }
