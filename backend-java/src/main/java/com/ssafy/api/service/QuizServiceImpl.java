40b7c668be404e61dbee3611debd9057726e3e63
남현우 <frozen_load7@naver.com>
2022-07-28 10:00:14 +0900

    0728-1

--- backend-java/src/main/java/com/ssafy/api/service/QuizServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/QuizServiceImpl.java
@@ -2,8 +2,10 @@ package com.ssafy.api.service;
 
 import com.ssafy.api.request.QuizBookCreateGetReq;
 import com.ssafy.db.entity.QuizBook;
+import com.ssafy.db.entity.User_QuizBook;
 import com.ssafy.db.repository.QuizBookRepository;
 import com.ssafy.db.repository.UserRepository;
+import com.ssafy.db.repository.User_QuizBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +14,21 @@ public class QuizServiceImpl implements  QuizService{
     @Autowired
     QuizBookRepository quizBookRepository;
 
+    @Autowired
+    User_QuizBookRepository user_quizBookRepository;
+
     @Override
     public QuizBook createQuizBook(QuizBookCreateGetReq quizBookCreateGetReq) {
         QuizBook quizBook = new QuizBook();
         quizBook.setQuizbookSize(0);
 
-        return quizBookRepository.save(quizBook) ;
+        //
+        quizBookRepository.save(quizBook) ;
+        User_QuizBook user_quizBook = new User_QuizBook();
+        user_quizBook.setUserId(quizBookCreateGetReq.getUserId());
+        user_quizBook.setQuizbookId(quizBook.getQuizbookId());
+        user_quizBookRepository.save(user_quizBook);
+        return quizBook;
     }
 
     @Override
