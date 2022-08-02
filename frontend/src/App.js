f04b6884f4cc7dbccda6e02fd034f97845404636
최영선 <vnddns26@naver.com>
2022-08-02 17:41:19 +0900

    frontend merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Homepage from "./pages/Homepage";
+import ProblemList from "./pages/ProblemList";
+import CreateQuestion from "./pages/CreateQuestion";
+import UserProfile from "./pages/UserProfile";
+import Navbar from "./components/Navbar";
+import Logout from "./pages/Logout";
+import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { Grid } from "@mui/material"; //contain
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            direction="column"
+          >
+            <Grid item xs={12} sm={12}>
+              <Navbar></Navbar>
+            </Grid>
+
+            <Routes>
+              <Route path="/" element={<Login></Login>}></Route>
+              <Route path="/signup" element={<Signup></Signup>}></Route>
+              <Route path="/homepage" element={<Homepage></Homepage>}></Route>
+              <Route
+                path="/problemlist"
+                element={<ProblemList></ProblemList>}
+              ></Route>
+              <Route
+                path="/createquestion"
+                element={<CreateQuestion></CreateQuestion>}
+              ></Route>
+              <Route
+                path="/userprofile"
+                element={<UserProfile></UserProfile>}
+              ></Route>
+              <Route
+              path="/logout"
+              element={<Logout></Logout>}
+            ></Route>
+            </Routes>
+          </Grid>
+        </PersistGate>
+      </Provider>
     </div>
   );
 }
--- frontend/src/pages/ProblemList.js
+++ frontend/src/pages/ProblemList.js
@@ -8,6 +8,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { Grid } from "@mui/material"; //contain
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -22,31 +23,37 @@ const rows = [
 
 function ProblemList() {
   return (
-    <div style={{ display: "flex" }}>
-      <TableContainer sx={{ maxWidth: 1200 }} component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead></TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell sx={{ minWidth: 1000 }} component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">
-                  <Button>삭제</Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <div style={{ position: "relative", left: 30, top: 50 }}>
-        <Button>문제 생성</Button>
-      </div>
-    </div>
+    <>
+      <Grid item container spacing={2}>
+        <Grid item xs={12} md={12}>
+          <Button>문제 생성</Button>
+        </Grid>
+
+        <Grid item xs={12} md={12}>
+          <TableContainer sx={{ maxWidth: 1200 }} component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>문제 리스트</TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell sx={{ minWidth: 1000 }} component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Button>삭제</Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        
+      </Grid>
+    </>
   );
 }
 export default ProblemList;
