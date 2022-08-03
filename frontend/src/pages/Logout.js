2d593e41d5ece83c9508691912f9e2dfe0ec9f26
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-03 17:41:11 +0900

    accessToken 접근 중간 저장

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -2,16 +2,16 @@ import { useNavigate } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteToken } from '../redux/token';
 import { useEffect } from 'react';
-
+import axios from 'axios';
 
 function Logout(){
     // store에 저장된 Access Token 정보를 받아 온다
     const token = useSelector(state => state.token.value);
-    console.log(token.accessToken);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     dispatch(deleteToken());
-    console.log(token.accessToken);
+    axios.defaults.headers.common["Authorization"] = null;
+    localStorage.removeItem("token");
     useEffect(()=>{
         if(token.accessToken ===""){
             navigate("/", { replace: true });
