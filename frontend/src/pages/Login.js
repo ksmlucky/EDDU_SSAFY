47b9af2a733f64e5f7f0eb1a541c823a22bb8b70
최영선 <vnddns26@naver.com>
2022-08-08 16:58:27 +0900

    modal 수정

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,9 +10,8 @@ import axios from "axios";
 import users from "../api/api";
 import { useDispatch } from "react-redux";
 import { setToken } from "../redux/token";
-import styles from '../css/login.module.css'
+import styles from "../css/login.module.css";
 import { me } from "../redux/user";
-
 
 const validationSchema = yup.object({
   password: yup.string("Enter your password").required("Password is required"),
@@ -53,7 +52,7 @@ function Login() {
           console.log(res.data);
           dispatch(me(res.data));
         });
-        navigate("/homepage", { replace: true });
+        navigate("/", { replace: true });
       });
     },
   });
@@ -61,123 +60,136 @@ function Login() {
   const Textfieldsx = {
     width: "70%",
     height: "100%",
-    "& .MuiInputLabel-root": {color: "black", fontSize:"0.8vmax",},
+    "& .MuiInputLabel-root": { color: "black", fontSize: "0.8vmax" },
     "& .MuiOutlinedInput-root": {
-    "& > fieldset": { width:"100%", height:"100%", border:"3px solid blue", borderRadius: "20px 20px"},
-  },
+      "& > fieldset": {
+        width: "100%",
+        height: "100%",
+        border: "3px solid blue",
+        borderRadius: "20px 20px",
+      },
+    },
     "& .MuiOutlinedInput-root:hover": {
-    "& > fieldset": {
-    borderColor: "blue"
-    }
-  },
+      "& > fieldset": {
+        borderColor: "blue",
+      },
+    },
     "& .MuiOutlinedInput-root.Mui-focused": {
-    "& > fieldset": {
-    borderColor: "blue"
-    }
-  }
-}
+      "& > fieldset": {
+        borderColor: "blue",
+      },
+    },
+  };
 
-const Buttonsx = {
+  const Buttonsx = {
     "&.MuiButton-root": {
-    border: "3px blue solid",
-    width: "80%",
-    textDecoration: "none",
-    borderRadius: "70px 70px",
-    padding: "10px 0px",
-    color: "#4C3657",
-  },
-  "&.MuiButton-root::before": {
-    content: "''",
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    borderRadius: "70px 70px",
-    backgroundColor: "#FDDD6D",
-    top: "-10px",
-    left: "10px",
-    zIndex: "-1",
-  },
-}
+      border: "3px blue solid",
+      width: "80%",
+      textDecoration: "none",
+      borderRadius: "70px 70px",
+      padding: "10px 0px",
+      color: "#4C3657",
+    },
+    "&.MuiButton-root::before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      borderRadius: "70px 70px",
+      backgroundColor: "#FDDD6D",
+      top: "-10px",
+      left: "10px",
+      zIndex: "-1",
+    },
+  };
 
   return (
     <div>
-    <div className={ styles.container}>
-      <div className={styles.toplinks}>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <div>
-          <Link to="/signup" className={ styles.link}>
-            <Button type="submit" className={ styles.buttons}>회원가입</Button>
-          </Link>
+      <div className={styles.container}>
+        <div className={styles.toplinks}>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <div>
+              <Link to="/signup" className={styles.link}>
+                <Button type="submit" className={styles.buttons}>
+                  회원가입
+                </Button>
+              </Link>
+            </div>
+          </form>
         </div>
-      </form>
-      </div>
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          formik.handleSubmit(event);
-        }}
-      >
-        <div>
-          <h2 className={styles.h2}>Login</h2>
-          <span className={styles.span}>To join our Eddu SSAFY community web site </span>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            formik.handleSubmit(event);
+          }}
+        >
+          <div>
+            <h2 className={styles.h2}>Login</h2>
+            <span className={styles.span}>
+              To join our Eddu SSAFY community web site{" "}
+            </span>
           </div>
 
           <div className={styles.textcon}>
-        <div>
-          <TextField
-            name="userId"
-            label="userId"
-            value={formik.values.userId}
-            onChange={formik.handleChange}
-            error={formik.touched.userId && Boolean(formik.errors.userId)}
-            helperText={formik.touched.userId && formik.errors.userId}
-            sx={Textfieldsx}
+            <div>
+              <TextField
+                name="userId"
+                label="userId"
+                value={formik.values.userId}
+                onChange={formik.handleChange}
+                error={formik.touched.userId && Boolean(formik.errors.userId)}
+                helperText={formik.touched.userId && formik.errors.userId}
+                sx={Textfieldsx}
+              />
+            </div>
+            <div>
+              <TextField
+                name="password"
+                type="password"
+                label="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                helperText={formik.touched.password && formik.errors.password}
+                sx={Textfieldsx}
+              />
+            </div>
+          </div>
+          <div className={styles.subcon}>
+            <Button type="submit" disabled={formik.isSubmitting} sx={Buttonsx}>
+              로그인
+            </Button>
+          </div>
+        </form>
 
-          />
-        </div>
-        <div>
-          <TextField
-            name="password"
-            type="password"
-            label="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-            sx={Textfieldsx}
-          />
-        </div>
-        </div>
-        <div className={styles.subcon}>
-          <Button type="submit" disabled={formik.isSubmitting} sx={Buttonsx}>
-            로그인
-          </Button>
-        </div>
-      </form>
-
- 
-
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <div>
-          <Link to="/signup" className={styles.linktext}>
-            <Button type="submit" sx={{
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div>
+            <Link to="/signup" className={styles.linktext}>
+              <Button
+                type="submit"
+                sx={{
                   "&.MuiButton-root": {
                     color: "black",
-                    fontSize:"0.8vmin",
+                    fontSize: "0.8vmin",
                   },
-              }}>비밀번호찾기</Button>
-          </Link>
-        </div>
-      </form>
+                }}
+              >
+                비밀번호찾기
+              </Button>
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
--- frontend/src/redux/quizbook.js
+++ frontend/src/redux/quizbook.js
@@ -6,20 +6,16 @@ export const quizbookSlice = createSlice({
   name: "quizbooklist",
   initialState,
   reducers: {
-      addquizbook: (state, action) => {
+    addquizbook: (state, action) => {
       const newquizbook = action.payload;
-      state.quizbook.push({
-      id : newquizbook,
-      })
-      },
-      changequizbook: (state, action) => {
-        
-      },
-      removequizbook: (state, action) => {
-        const id = action.payload;
-        state.quizbook = state.quizbook.filter((item) => item.id !== id);
-      },
+      state.quizbook.push(newquizbook);
     },
+    changequizbook: (state, action) => {},
+    removequizbook: (state, action) => {
+      const id = action.payload;
+      state.quizbook = state.quizbook.filter((item) => item.quizbookId !== id);
+    },
+  },
 });
 
 export const quizbookActions = quizbookSlice.actions;
