7e324b0bbc22c10b1bd4d89f3436d5e0a079a868
frozen_load7 <frozen_load7@naver.com>
2022-08-10 16:07:37 +0900

    방 api 추가

--- backend-java/src/main/java/com/ssafy/api/service/UserRoomServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserRoomServiceImpl.java
@@ -2,7 +2,9 @@ package com.ssafy.api.service;
 
 import com.ssafy.api.request.UserRoomReq;
 import com.ssafy.api.response.RoomRes;
+import com.ssafy.api.response.UserRes;
 import com.ssafy.db.entity.UserRoom;
+import com.ssafy.db.repository.RoomRepository;
 import com.ssafy.db.repository.UserRoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,18 @@ public class UserRoomServiceImpl implements UserRoomService{
 
     @Autowired
     UserRoomRepository userRoomRepository;
-    @Override
-    public boolean register(UserRoomReq userRoomReq) {
 
-        try{
+    @Autowired
+    RoomService roomService;
+
+    @Override
+    public boolean enterRoom(UserRoomReq userRoomReq) {
+
+        try{ //입장하는 유저가 호스트라면 방 start.
+            String hostId = roomService.getRoomById(userRoomReq.getRoomId()).getHostId();
+            if(hostId.equals(userRoomReq.getUserId())){
+                roomService.startRoom(userRoomReq);
+            }
             userRoomRepository.save(userRoomReq.toEntity());
         } catch(Exception e){
             e.printStackTrace();
@@ -30,8 +40,14 @@ public class UserRoomServiceImpl implements UserRoomService{
     }
 
     @Override
-    public boolean delete(UserRoomReq userRoomReq) {
-        try{
+    public boolean quitRoom(UserRoomReq userRoomReq) {
+        try{ //퇴장하는 유저가 호스트라면 방 end.
+            String hostId = roomService.getRoomById(userRoomReq.getRoomId()).getHostId();
+            if(hostId.equals(userRoomReq.getUserId())){
+                System.out.println("꼬북이");
+                roomService.endRoom(userRoomReq);
+            }
+            System.out.println("버터풀");
             userRoomRepository.deleteByRoomRoomIdAndUserUserId(userRoomReq.getRoomId(), userRoomReq.getUserId());
         } catch(Exception e){
             e.printStackTrace();
@@ -53,5 +69,20 @@ public class UserRoomServiceImpl implements UserRoomService{
             return null;
         }
         return rooms;
+    }
+
+    @Override
+    public List<UserRes> getUsersByRoomId(long roomId) {
+        List<UserRes> users = new ArrayList<>();
+        try{
+            List<UserRoom> userRooms = userRoomRepository.findByRoomRoomId(roomId);
+            for(UserRoom ur : userRooms){
+                users.add(UserRes.of(ur.getUser()));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return users;
     }
 }
