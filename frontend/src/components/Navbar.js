0a176f0180a133da4aec518c55e7c6a0b2906dcc
Kim-Ji-soo <67624305+Kim-Ji-soo@users.noreply.github.com>
2022-08-11 10:47:07 +0900

    Navbar 문제생성 제거

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -35,9 +35,6 @@ export default function Navbar() {
               <Link to="/problemlist" className={classes.link}>
                 문제 목록
               </Link>
-              <Link to="/createquestion" className={classes.link}>
-                문제 생성
-              </Link>
               <Link to="/userprofile" className={classes.link}>
                 개인정보 수정
               </Link>
